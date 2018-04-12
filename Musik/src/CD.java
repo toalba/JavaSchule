49debd7a965b770008a129e173bda0d76969a752
Torben Baumgartner <tb@kleinundpartner.at>
2018-04-12 12:16:22 +0200

    musikbox

---
+++ Musik/src/CD.java
@@ -0,0 +1,20 @@
+
+public class CD {
+	private String info;
+	
+	public
+	
+	public static void addsong(Song song)
+	{
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Song[] song = new Song[30];
+		
+
+	}
+
+}
---
+++ Musik/src/Song.java
@@ -0,0 +1,18 @@
+
+public class Song {
+	
+	private double laenge;
+	private String titel;
+	
+	public Song(double l, String t)
+	{
+		this.laenge= l;
+		this.titel = t;
+	}
+	public void ausgabe()
+	{
+		System.out.printf("Titel: %s Länge: %f",titel,laenge);
+	}
+	
+
+}
