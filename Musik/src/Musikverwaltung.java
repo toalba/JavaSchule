cad40d9e9de619135f1333c6854098d6200eaa0f
toalba <tb@kleinundpartner.at>
2018-04-30 15:23:22 +0200

    musikverwaltung

--- Musik/src/Musikverwaltung.java
+++ Musik/src/Musikverwaltung.java
@@ -16,6 +16,7 @@ public class Musikverwaltung {
 	}
 	public void ausgabe()
 	{
+		System.out.println();
 		for (int i = 0; i < pos; i++) {
 			CD cd = cds[i];
 			cd.ausgabe();
@@ -32,28 +33,5 @@ public class Musikverwaltung {
         cds[loeschPos] = cds[pos-1];
         loeschen();       
     }
-
-	    
-
-	public static void main(String[] args) {
-		
-		
-		CD acdc = new CD("AC|DC");
-		CD camora =new CD("RAF Camora");
-		Song hwh= new Song(3.28,"High Way to Hell");
-		Song pri= new Song(3.23,"Primo");
-		Song omt= new Song(3.08,"Ohne mein Team");
-		Song gc= new Song(2.27,"Gotham City");
-		
-		acdc.addsong(hwh);
-		camora.addsong(pri);
-		camora.addsong(omt);
-		camora.addsong(gc);
-		
-        
-		
-		
-	}
-	
 
 }
