7280e2e73dabcc271b019fc581f076984e1c9cc6
Toalba <tb@kleinundpartner.at>
2019-09-12 13:16:16 +0200

    3A

--- Poker/src/Give.java
+++ Poker/src/Give.java
@@ -1,6 +1,39 @@
 
 public class Give {
 
-    private int[] Carddeck= new int[51];
+    private static int handrange = 5;
+    private int[] cardDeck= new int[51];
+    private int[] hand = new int[handrange];
     
+    public void fill()
+    {
+        for(int i=0;i<cardDeck.length;i++)
+        {
+            cardDeck[i]=i;
+        }
+    }
+
+    public void gettoHand()
+    {
+        
+      
+
+        for(int i = 0; i<handrange;i++)
+        {
+            int lenght = cardDeck.length - thrownCards;
+            int thrownCards= 0;
+            int random = (int)(Math.random()*lenght);
+            
+            hand[i]= cardDeck[random];
+            cardDeck[random]=cardDeck[cardDeck.length-thrownCards];
+            thrownCards++;
+
+
+        }
+
+
+
+    }
+
+
 }
