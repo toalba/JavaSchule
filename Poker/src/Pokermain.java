c3e29016ba6cc47f27eb3a5259175d4b3376ff7d
Toalba <tb@kleinundpartner.at>
2019-09-19 13:12:38 +0200

    pokermain

--- Poker/src/Pokermain.java
+++ Poker/src/Pokermain.java
@@ -4,7 +4,17 @@ public class Pokermain {
 	public static void main(String[] args) {
 		Give gg =new Give(); 
 		int analyticTimes=1000;
-		
+		// Kartencounter
+		int highcardCounter=0;
+		int pairCounter=0;
+		int trippleCounter=0;
+		int fourthCounter=0;
+		int fullHouseCounter=0;
+		int flushCounter=0;
+		int straightCounter=0;
+		int straightflushCounter=0;
+		int royalflush=0;
+
 	}
 
 }
