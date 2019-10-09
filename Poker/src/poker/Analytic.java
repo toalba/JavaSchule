c6e5d61dfc0dfee5cc0e1b307bb31572cd1c4400
Toalba <tb@kleinundpartner.at>
2019-10-10 00:02:04 +0200

    jetzt isses rank und schlank

---
+++ Poker/src/poker/Analytic.java
@@ -0,0 +1,107 @@
+package poker;
+
+public class Analytic{
+    private double analyticTimes=10000000;
+    private double highcardCounter=0;
+    private double pairCounter=0;
+    private double trippleCounter=0;
+    private double squadCounter=0;
+    private double doublePairCounter=0;
+    private double fullHouseCounter=0;
+    private double flushCounter=0;
+    private double straightCounter=0;
+    private double straightflushCounter=0;
+    private double royalflush=0;
+
+    public void analyse()
+    {
+        for(int i=0;i<analyticTimes;i++)
+		{
+			Give gg =new Give(); 
+			boolean abbr= true;
+			gg.gettoHand();
+			
+			if(gg.royalFlush() && abbr)
+			{
+				royalflush++;
+				abbr=false;
+			}
+			if(gg.straightFlushCeck() && abbr)
+			{
+				straightflushCounter++;
+				abbr=false;
+			}
+			if(gg.squad()&& abbr){
+				
+				squadCounter++;
+				abbr=false;
+			}
+			if(gg.flush() && abbr)
+			{
+				flushCounter++;
+				abbr=false;
+			}
+			if(gg.straight() && abbr)
+			{
+				straightCounter++;
+				abbr=false;
+			}
+
+			if(gg.fullHouse()&& abbr)
+			{
+				fullHouseCounter++;
+				abbr=false;
+			}
+			if(gg.doublePair()&& abbr)
+			{
+				doublePairCounter++;
+				abbr=false;
+			}
+			if(gg.tripple()&& abbr)
+			{
+				trippleCounter++;
+				abbr=false;
+			}
+			if(gg.onePair()&& abbr)
+			{
+				pairCounter++;
+				abbr=false;
+			}
+			if(abbr)
+			{
+				highcardCounter++;
+				abbr= false;
+			}
+        }
+    }    
+    public void output()
+    {
+        System.out.print("Anzahl an Versuche:");
+		System.out.println(analyticTimes);
+		System.out.print("Gesamtanzahl:");
+		System.out.println(flushCounter+straightCounter+straightflushCounter+pairCounter+trippleCounter+royalflush+doublePairCounter+highcardCounter+fullHouseCounter+squadCounter);
+		System.out.printf("Flush:%f",(flushCounter*100)/analyticTimes);
+		System.out.println("%");
+		System.out.printf("Straight:%f",(straightCounter*100)/analyticTimes);
+		System.out.println("%");
+		System.out.printf("Pairs:%f",(pairCounter*100)/analyticTimes);
+		System.out.println("%");
+		System.out.printf("Tripple: %f",(trippleCounter*100)/analyticTimes);
+		System.out.println("%");
+		System.out.printf("RoyalFlush: %f",(royalflush*100)/analyticTimes);
+		System.out.println("%");
+		System.out.printf("Doublepair: %f",(doublePairCounter*100)/analyticTimes);
+		System.out.println("%");
+		System.out.printf("HighCard: %f",(highcardCounter*100)/analyticTimes);
+		System.out.println("%");
+		System.out.printf("Fullhouse: %f",(fullHouseCounter*100)/analyticTimes);
+		System.out.println("%");
+		System.out.printf("Squad: %f",(squadCounter*100)/analyticTimes);
+		System.out.println("%");
+		System.out.printf("StraightFlush: %f", (straightflushCounter*100)/analyticTimes);
+		System.out.println("%");
+    }
+    
+
+
+}
--- Poker/src/poker/Pokermain.java
+++ Poker/src/poker/Pokermain.java
@@ -4,104 +4,13 @@ public class Pokermain {
 
 	public static void main(String[] args) {
 		
-		double analyticTimes=10000000;
-		// Kartencounter
-		double highcardCounter=0;
-		double pairCounter=0;
-		double trippleCounter=0;
-		double squadCounter=0;
-		double doublePairCounter=0;
-		double fullHouseCounter=0;
-		double flushCounter=0;
-		double straightCounter=0;
-		double straightflushCounter=0;
-		double royalflush=0;
-		
-		for(int i=0;i<analyticTimes;i++)
-		{
-			Give gg =new Give(); 
-			boolean abbr= true;
-			gg.gettoHand();
-			
-			if(gg.royalFlush() && abbr)
-			{
-				royalflush++;
-				abbr=false;
-			}
-			if(gg.straightFlushCeck() && abbr)
-			{
-				straightflushCounter++;
-				abbr=false;
-			}
-			if(gg.squad()&& abbr){
-				
-				squadCounter++;
-				abbr=false;
-			}
-			if(gg.flush() && abbr)
-			{
-				flushCounter++;
-				abbr=false;
-			}
-			if(gg.straight() && abbr)
-			{
-				straightCounter++;
-				abbr=false;
-			}
+		Analytic Game = new Analytic();
 
-			if(gg.fullHouse()&& abbr)
-			{
-				fullHouseCounter++;
-				abbr=false;
-			}
-			if(gg.doublePair()&& abbr)
-			{
-				doublePairCounter++;
-				abbr=false;
-			}
-			if(gg.tripple()&& abbr)
-			{
-				trippleCounter++;
-				abbr=false;
-			}
-			if(gg.onePair()&& abbr)
-			{
-				pairCounter++;
-				abbr=false;
-			}
-			if(abbr)
-			{
-				highcardCounter++;
-				abbr= false;
-			}
-		}
+		//Analyse
+		Game.analyse();
+		//Ausgabe
+		Game.output();
 
-		// Analytics
-		// Controll
-		System.out.print("Anzahl an Versuche:");
-		System.out.println(analyticTimes);
-		System.out.print("Gesamtanzahl:");
-		System.out.println(flushCounter+straightCounter+straightflushCounter+pairCounter+trippleCounter+royalflush+doublePairCounter+highcardCounter+fullHouseCounter+squadCounter);
-		System.out.printf("Flush:%f",(flushCounter*100)/analyticTimes);
-		System.out.println("%");
-		System.out.printf("Straight:%f",(straightCounter*100)/analyticTimes);
-		System.out.println("%");
-		System.out.printf("Pairs:%f",(pairCounter*100)/analyticTimes);
-		System.out.println("%");
-		System.out.printf("Tripple: %f",(trippleCounter*100)/analyticTimes);
-		System.out.println("%");
-		System.out.printf("RoyalFlush: %f",(royalflush*100)/analyticTimes);
-		System.out.println("%");
-		System.out.printf("Doublepair: %f",(doublePairCounter*100)/analyticTimes);
-		System.out.println("%");
-		System.out.printf("HighCard: %f",(highcardCounter*100)/analyticTimes);
-		System.out.println("%");
-		System.out.printf("Fullhouse: %f",(fullHouseCounter*100)/analyticTimes);
-		System.out.println("%");
-		System.out.printf("Squad: %f",(squadCounter*100)/analyticTimes);
-		System.out.println("%");
-		System.out.printf("StraightFlush: %f", (straightflushCounter*100)/analyticTimes);
-		System.out.println("%");
 
 	}
 
